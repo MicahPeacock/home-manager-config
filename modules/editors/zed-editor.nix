3a30e8108e348dfdba28b77222043bd049d70d90
MicahPeacock <micah.peacock@proton.me>
2025-11-26 02:22:58 -0700

    Renamed zed to zed-editor (full name)

--- modules/editors/zed-editor.nix
+++ modules/editors/zed-editor.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.zed = {
+  programs.zed-editor = {
     enable = true;
-    package = config.lib.nixGL.wrap pkgs.zed;
+    package = config.lib.nixGL.wrap pkgs.zed-editor;
   };
 
   home.file.".config/zed/settings.json".source = ../../files/.config/zed/settings.json;
